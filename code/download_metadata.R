1d1541fdb71f14d33e7fb7bffbaa31e07949b5ef
gzahn <geoff.gone@gmail.com>
2026-08-20 12:51:30 -0400

    add metadata dl script

---
+++ code/download_metadata.R
@@ -0,0 +1,42 @@
+# downloads current metadata sheets from Google Drive
+# Need to rerun after any metadata updates
+
+library(googlesheets4)
+
+# No authentication needed for public sheets
+gs4_deauth()
+
+# add links
+
+## phase 1 metadata
+urls <- c(
+  sites = "https://docs.google.com/spreadsheets/d/1SU2VYWOQ-Ob2JnhVqs7GiR-KVRb4xs5JJd_yMFyj7eo/edit?usp=sharing",
+  seqs = "https://docs.google.com/spreadsheets/d/1H90WX5jfnVeueVUSXBB9HPUYTIEFCUKzIyGH2Maqk4s/edit?usp=sharing",
+  samps = "https://docs.google.com/spreadsheets/d/1q5PAT23KhXMSpy2DKXyaphIih_a565WYoQa4LuY8deo/edit?usp=sharing",
+  greenhouse = "https://docs.google.com/spreadsheets/d/1kTVpIZFeIH0gN4Ma4cyJSEZawCaM4ikM22relD0V9DY/edit?usp=sharing",
+  expunits = "https://docs.google.com/spreadsheets/d/1tYB1SBKKvQOqlxcwDsKKiXmRcYoVFKzEokfi_BamcNQ/edit?usp=sharing"
+)
+
+# Import all sheets into a named list of data frames
+dat <- lapply(urls, read_sheet)
+
+# Create individual data frames
+list2env(dat, envir = .GlobalEnv)
+
+# write out to files in ./data/phase1/
+filenames <- c(
+  sites = "metadata_phase-1_sites.csv",
+  seqs = "metadata_phase-1_sequencing.csv",
+  samps = "metadata_phase-1_samples.csv",
+  greenhouse = "metadata_phase-1_greenhouse.csv",
+  expunits = "metadata_phase-1_experimental_units.csv"
+)
+
+mapply(
+  function(df, file) {
+    write.csv(df, file.path("./data/phase1", file), row.names = FALSE)
+  },
+  dat[names(filenames)],
+  filenames,
+  SIMPLIFY = FALSE
+)
---
+++ workflow_phase1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# download most recent metadata from google sheets
+# phase 1 metadata:
+bash ./data/phase1/download_metadata.R
+# phase 2 metadata:
+	# placeholder
+# 
